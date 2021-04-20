19b8eeb5f991f00d64cb8c32da9ac08fb079a8e8
Joe Lee <75293272+joeslee94@users.noreply.github.com>
2021-04-20 20:05:58 +0900

    Add files via upload

---
+++ UdemyProjects/IterationZero/Quote.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QAPI.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Order { get; set; }
+        public string Description { get; set; }
+
+        //build to see if it works
+    }
+}
